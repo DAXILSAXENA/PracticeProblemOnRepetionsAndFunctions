d1624880e84ffe863d879c4b8010c340a335f35b
DAXILSAXENA <daxildaxil@gmail.com>
2021-06-01 15:35:43 +0530

    Calculate prime factors of a number

---
+++ PrimeFactorsForLoop.sh
@@ -0,0 +1,20 @@
+#! /bin/bash -x
+
+# Calculate prime factors of a number
+
+read -p "Enter the number to compute prime factor :" number
+
+while [ $(( number % 2 )) -eq 0 ]
+do
+        echo -n "2 "
+        number=$(( number / 2 ))
+done
+
+for (( i=3; i<=number; i=i + 2 ))
+do
+        while [ $(( number % i )) -eq 0 ]
+        do
+                echo -n "$i "
+            number=$(( number / i ))
+        done
+done
